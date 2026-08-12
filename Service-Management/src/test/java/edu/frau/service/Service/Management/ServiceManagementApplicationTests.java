654ba38e2d14fea91b590a5f42623fbc77a68056
BidhanPaul <bidhanpaul7@gmail.com>
2026-08-12 23:14:22 +0200

    Use H2 in-memory database for tests via test profile

--- Service-Management/src/test/java/edu/frau/service/Service/Management/ServiceManagementApplicationTests.java
+++ Service-Management/src/test/java/edu/frau/service/Service/Management/ServiceManagementApplicationTests.java
@@ -2,12 +2,14 @@ package edu.frau.service.Service.Management;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ServiceManagementApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
